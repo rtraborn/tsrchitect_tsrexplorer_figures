a672f3bbf0908a939144274d36acba81a03db1fa
Shannon Snyder <ssnyde11@asu.edu>
2020-04-07 14:53:53 -0700

    added scripts for Agave

---
+++ launch_job_tsrs.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+ 
+#SBATCH -n 1                        # number of cores
+#SBATCH -t 0-12:00                  # wall time (D-HH:MM)
+##SBATCH -A ssnyde11             # Account hours will be pulled from (commented out with double # in front)
+#SBATCH -o slurm.%j.out             # STDOUT (%j = JobId)
+#SBATCH -e slurm.%j.err             # STDERR (%j = JobId)
+#SBATCH --mem=48GB
+
+##SBATCH --mail-type=ALL             # Send a notification when the job starts, stops, or fails
+##SBATCH --mail-user=myemail@asu.edu # send-to address
+
+myDir=/home/ssnyde11/scratch/march_tsrchitect/tsr
+
+echo "Launching job"
+
+cd $myDir
+
+./xrunSwf > err
+
+echo "Job complete"
